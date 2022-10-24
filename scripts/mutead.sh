cc873f9aca3494738ae88b3605d8534636d0109b
Koray Aydemir <korayaydemir276@gmail.com>
2022-10-24 04:40:29 +0300

    mutead

---
+++ scripts/mutead.sh
@@ -0,0 +1,17 @@
+#if playerctl metadata --format "{{mpris:trackid}}" | grep -q "/ad"
+#then amixer set Master toggle
+#else :
+#fi
+OUTPUT=$(playerctl metadata --format "{{ volume }}")
+# if the metadata trackid has ad in it every second
+if playerctl metadata --format "{{mpris:trackid}}" | grep -q "/ad"
+# then get the player's volume store it, set all players volume to 0
+then OUTPUT=$(playerctl metadata --format "Volume: {{ volume * 100 }}"), 
+playerctl --all-players volume 0;
+echo "then";
+else playerctl --all-players volume $((OUTPUT))
+  echo OUTPUT;
+  echo "else";
+fi
+
+
